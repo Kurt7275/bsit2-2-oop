132fac2a66ad7290ac32ede79a8eff1ee05a0f86
Xierick <kbautista99931@liceo.edu.ph>
2025-09-23 11:26:23 +0800

    Create DVD.java

---
+++ src/DVD.java
@@ -0,0 +1,18 @@
+public class DVD extends Media {
+    private String director;
+
+    public DVD(String title, String itemId, String director) {
+        super(title, itemId);
+        this.director = director;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "DVD";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("DVD - Title: " + title + ", Director: " + director + ", ID: " + itemId);
+    }
+}
