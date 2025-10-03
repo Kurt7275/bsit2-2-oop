a8057556f5ea3537bc6ff82159838d95fb2d8caf
Xierick <kbautista99931@liceo.edu.ph>
2025-10-03 09:59:47 +0800

    Update Borrowable.java

--- src/Borrowable.java
+++ src/Borrowable.java
@@ -3,6 +3,7 @@ public interface Borrowable {
     void returnItem();
     boolean isAvailable();
     int getBorrowingPeriod(); // in days
+
     default String getBorrowingStatus() {
         return isAvailable() ? "Available for borrowing" : "Currently borrowed";
     }
