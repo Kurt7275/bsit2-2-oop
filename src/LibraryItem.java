6f727b47af2105b5f60505132b4ae446aefcfafd
Xierick <kbautista99931@liceo.edu.ph>
2025-10-03 10:00:18 +0800

    Update LibraryItem.java

--- src/LibraryItem.java
+++ src/LibraryItem.java
@@ -4,6 +4,7 @@ public abstract class LibraryItem {
     protected String author;
     protected boolean isCheckedOut;
     protected String borrowerName;
+
     public LibraryItem(String itemId, String title, String author) {
         this.itemId = itemId;
         this.title = title;
@@ -11,29 +12,21 @@ public abstract class LibraryItem {
         this.isCheckedOut = false;
         this.borrowerName = null;
     }
-    public String getItemId() {
-        return itemId;
-    }
+
     public String getItemInfo() {
-        String status = isCheckedOut ? "Checked out by " + borrowerName : "Available";
-        return String.format("[%s] %s by %s - %s", itemId, title, author, status);
+        return String.format("%s by %s", title, author);
     }
+
     public void checkOut(String borrowerName) {
-        if (isCheckedOut) {
-            System.out.println("Item " + itemId + " is already checked out.");
-            return;
-        }
         this.isCheckedOut = true;
         this.borrowerName = borrowerName;
     }
+
     public void checkIn() {
-        if (!isCheckedOut) {
-            System.out.println("Item " + itemId + " is not currently checked out.");
-            return;
-        }
         this.isCheckedOut = false;
         this.borrowerName = null;
     }
+
     public abstract String getItemType();
     public abstract double calculateLateFee(int daysLate);
 }
